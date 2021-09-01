47dadc1f5afc41addae71698ce156d3e9ec50b39
Dinimar Fish <tayla@tallisman.com.au>
2021-09-01 21:57:15 +1000

    created association  methods in models/index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+}),
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  //Defines the third table needed to store the foreign keys.
+   through: {
+     model: ProductTag,
+     unique: false
+   },
+   as: 'product_codes'   
+ });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  //Defines the third table needed to store the foreign keys.
+   through: {
+     model: ProductTag,
+     unique: false
+   },
+   as: 'tag_codes'   
+ });
 
 module.exports = {
   Product,
